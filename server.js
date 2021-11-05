2e4e0b9ca2bbf00141b7e4bed4f9a1e6072698a0
Jalon Tuttle <jalontuttle@gmail.com>
2021-11-05 14:47:30 -0400

    adding db and routes folder. main html page linked

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const path = require('path');
-const fs = require("fs")
+// const apiRoute = require("./routes/apiroute");
+const htmlRoute = require("./routes/indexroute");
 
 const PORT = process.env.port || 3001;
 
@@ -13,14 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-); 
+// apiRoute(app);
+htmlRoute(app);
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
