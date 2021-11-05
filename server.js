44bf47267a2770776da8232b57968524b6b9b915
Jalon Tuttle <jalontuttle@gmail.com>
2021-11-05 09:48:12 -0400

    deleting unnecessary files and restarting

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const api = require('./Develop/public/assets/js/index.js');
+const fs = require("fs")
 
 const PORT = process.env.port || 3001;
 
@@ -10,16 +10,15 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, 'notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 ); 
 
 app.listen(PORT, () =>
